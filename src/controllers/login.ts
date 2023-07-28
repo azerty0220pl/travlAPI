ea7556a6391865ae0f7ab51e70ebbd02963247fb
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-07-28 18:46:07 +0200

    login controlled updated

---
+++ src/controllers/login.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { sign } from 'jsonwebtoken';
+import userService from '../services/user';
+import 'dotenv/config';
+import UserModel from '../models/user';
+
+const loginController = (req: Request, res: Response) => {
+    userService.fetchOne(req.body.username).then(user => {
+        if (user && req.body.password === (user as UserModel).password)
+            res.status(202).json({
+                user: user,
+                token: sign(String((user as UserModel).id), process.env.TOKEN_KEY!)
+            });
+        else
+            return res.status(401).json({ error: true, message: "Not Authorized" });
+    })
+}
+
+export default loginController;
