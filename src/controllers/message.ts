8e3ba2917a849fc3ff1021ea25ae5b240bae370e
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-07-31 14:17:44 +0200

    message controller with mongo

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -1,8 +1,51 @@
 import { Request, Response } from 'express';
-import messages from '../data/messages.json';
+import messageService from '../services/message';
 
-const messageController = (_req: Request, res: Response) => {
-    res.json({ messages: messages });
+interface Orders { [key: string]: { [key: string]: 1 | -1 } };
+const orders: Orders = {
+    date: { date: 1 }
 }
+
+interface Filters { [key: string]: object };
+const filters: Filters = {
+    all: {},
+    read: { read: false, archived: false },
+    archived: { archived: false }
+}
+
+const messageController = {
+    getPage: (req: Request, res: Response) => {
+        messageService.fetchPage(parseInt(req.query.page as string) || 0, parseInt(req.query.limit as string) || 10, filters[req.query.filter as string || "all"], orders[req.query.order as string || "date"]).then(x => {
+            if (typeof x === 'string')
+                return res.json({ error: true, message: x });
+            else
+                return res.json({ message: x });
+        });
+    },
+    getById: (req: Request, res: Response) => {
+        messageService.fetchById(req.params.id).then(x => {
+            if (typeof x === 'string')
+                return res.json({ error: true, message: x });
+            else
+                return res.json({ message: x });
+        });
+    },
+    update: (req: Request, res: Response) => {
+        messageService.update(req.body.message).then(x => {
+            if (x === 'Message updated')
+                return res.json({ error: false, message: x });
+            else
+                return res.json({ error: true, message: x });
+        });
+    },
+    new: (req: Request, res: Response) => {
+        messageService.new(req.body.message).then(x => {
+            if (x === 'Message saved')
+                return res.json({ error: false, message: x });
+            else
+                return res.json({ error: true, message: x });
+        });
+    }
+};
 
 export default messageController;
