0167a325c04351db8a279cc9b0a7a56b9e74ea18
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-07-31 11:01:32 +0200

    User enhaced

--- src/services/user.ts
+++ src/services/user.ts
@@ -17,78 +17,80 @@ const User = model("User", userSchema);
 
 const userService = {
     fetchOne: async (name: String): Promise<UserModel | string> => {
-        let res: UserModel | string = "Not found";
+        try {
+            await User.findOne({ name: name }).then(doc => {
+                if (doc) {
+                    return {
+                        id: doc.id,
+                        name: doc.name,
+                        phone: doc.phone,
+                        email: doc.email,
+                        description: doc.description,
+                        joined: doc.joined,
+                        password: doc.password,
+                        status: doc.left ? false : true
+                    };
+                }
+            });
+        } catch {
+            return "Database error"
+        }
 
-        await User.findOne({ name: name }).then(doc => {
-            if (doc) {
-                res = {
-                    id: doc.id,
-                    name: doc.name,
-                    phone: doc.phone,
-                    email: doc.email,
-                    description: doc.description,
-                    joined: doc.joined,
-                    password: doc.password,
-                    status: doc.left ? false : true
-                };
-            }
-        });
-
-        return res;
+        return "Not found";
     },
     fetchById: async (id: String): Promise<UserModel | string> => {
-        let res: UserModel | string = "Not found";
+        try {
+            await User.findById(id).then(doc => {
+                if (doc) {
+                    return {
+                        id: doc.id,
+                        name: doc.name,
+                        phone: doc.phone,
+                        email: doc.email,
+                        description: doc.description,
+                        joined: doc.joined,
+                        password: doc.password,
+                        status: doc.left ? false : true
+                    };
+                }
+            });
+        } catch {
+            return "Database error"
+        }
 
-        await User.findById(id).then(doc => {
-            if (doc) {
-                res = {
-                    id: doc.id,
-                    name: doc.name,
-                    phone: doc.phone,
-                    email: doc.email,
-                    description: doc.description,
-                    joined: doc.joined,
-                    password: doc.password,
-                    status: doc.left ? false : true
-                };
-            }
-        });
-
-        return res;
+        return "Not found";
     },
     fetchPage: async (page: number, limit: number, filter: object, order: { [key: string]: 1 | -1 }): Promise<UserModel[] | string> => {
-        let res: UserModel[] | string = "Not found";
-
         try {
             let docs = await User.find(filter, {}, { skip: page * (limit - 1), limit: limit }).sort(order);
-                res = docs.map(el => {
-                    return {
-                        id: el.id,
-                        name: el.name,
-                        phone: el.phone,
-                        email: el.email,
-                        description: el.description,
-                        joined: el.joined,
-                        status: el.left ? false : true
-                    };
+            return docs.map(el => {
+                return {
+                    id: el.id,
+                    name: el.name,
+                    phone: el.phone,
+                    email: el.email,
+                    description: el.description,
+                    joined: el.joined,
+                    status: el.left ? false : true
+                };
             });
-        } catch {}
-
-        return res;
+        } catch {
+            return "Database error"
+        }
     },
     update: async (name: string, user: UserModel): Promise<string> => {
-        let res = "Not updated";
+        try {
+            await User.findOneAndUpdate({ name: name }, user).then(doc => {
+                if (doc)
+                    return "User updated";
+            });
+        } catch {
+            return "Database error"
+        }
 
-        await User.findOneAndUpdate({ name: name }, user).then(doc => {
-            if (doc)
-                res = "User updated";
-        });
-
-        return res;
+        return "Not updated";
     },
     new: async (user: UserModel): Promise<string> => {
-        let res = "Could not save user";
-
         try {
             let doc = await User.findOne({ username: user.name });
 
@@ -96,14 +98,13 @@ const userService = {
                 let us = new User(user);
 
                 await us.save()
-                res = "User saved";
+                return "User saved";
             } else {
-                res = "User already exist";
+                return "User already exist";
             }
-        } catch { }
-
-
-        return res;
+        } catch {
+            return "Database error"
+        }
     }
 };
 
