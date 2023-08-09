522b21bf27d221aa9ea646b2e84c9ce458175bde
Szymon Kokot <60388922+azerty0220pl@users.noreply.github.com>
2023-08-09 12:15:53 +0200

    connect to database removed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,5 @@
 import app from './app';
-import connect from './services/connect';
 const http = require('http');
 
-
-connect().then(() => {
-    console.log("Connected to database");
-});
 const server = http.createServer(app);
 server.listen(5000, () => {console.log("Server started on port 5000")});
